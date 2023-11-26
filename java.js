3f941931480e43fd4efb041c6f13ef7855d53164
juanaromeroniemes <150475191+juanaromeroniemes@users.noreply.github.com>
2023-11-26 19:15:41 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+const fabars=document.querySelector(".fa-solid");
+const ul=document.querySelector(".ul");
+fabars.addEventListener("click", function (){
+    ul.classList.toggle("lista");
+});
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
+function changeSlide(n) {
+    for (var i = 0; i < imgs.length; i++) { // reset
+      imgs[i].style.opacity = 0;
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+  
+    currentImg = n;
+  
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+  }
